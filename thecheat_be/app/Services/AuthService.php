39b87c143139c49bd896bfa4a2ead2bb452057a9
deltam3 <practicect@proton.me>
2025-06-04 14:30:33 +0900

    added https to isAuth cookie

--- thecheat_be/app/Services/AuthService.php
+++ thecheat_be/app/Services/AuthService.php
@@ -131,7 +131,7 @@ class AuthService
               'token' => $token,
             //   'user' => $user,
           ], 200)
-          ->cookie('isAuthenticated', 'true', 5256000, '/', '.thecheat.vercel.app', true, false, false, 'None');
+          ->cookie('isAuthenticated', 'true', 5256000, '/', 'https://thecheat.vercel.app', true, false, false, 'None');
         //   ->cookie('access_token', $token, $minutes, '/', '.example.com', true, true, false, 'None');
 
   
