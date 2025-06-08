d4bdd408d8ed00ca48bb95220307626d582dd249
deltam3 <practicect@proton.me>
2025-06-08 23:34:46 +0900

    imageresize 처음 코드로

--- thecheat_be/app/Jobs/ResizeProfileImage.php
+++ thecheat_be/app/Jobs/ResizeProfileImage.php
@@ -12,9 +12,9 @@ use Illuminate\Queue\SerializesModels;
 
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
-//use Intervention\Image\Laravel\Facades\Image;
-use Intervention\Image\ImageManager;
-use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\Laravel\Facades\Image;
+// use Intervention\Image\ImageManager;
+// use Intervention\Image\Drivers\Gd\Driver;
 class ResizeProfileImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
@@ -41,7 +41,7 @@ class ResizeProfileImage implements ShouldQueue
      */
     public function handle()
     {
-        /*
+
         $image = Storage::disk('public')->get($this->imagePath);
 
         $image = Image::read($image)->resize(180, 180);
@@ -52,14 +52,14 @@ class ResizeProfileImage implements ShouldQueue
 
         $this->userProfile->profile_image = 'profiles/resized/' . $resizedImageName;
         $this->userProfile->save();
-        */
 
-        $imageManager = new ImageManager(Driver::class);
-        $originalImage = $imageManager->read(Storage::disk('public')->path($this->imagePath));
-        $image = $originalImage->resize(180,180);
-        $resizedImageName = basename($this->imagePath, '.' . pathinfo($this->imagePath, PATHINFO_EXTENSION)) . '-resized.jpg';
-        Storage::disk('public')->put('profiles/resized/' . $resizedImageName, (string) $image->encode());
-        $this->userProfile->profile_image = 'profiles/resized/' . $resizedImageName;
-        $this->userProfile->save();
+
+        // $imageManager = new ImageManager(Driver::class);
+        // $originalImage = $imageManager->read(Storage::disk('public')->path($this->imagePath));
+        // $image = $originalImage->resize(180,180);
+        // $resizedImageName = basename($this->imagePath, '.' . pathinfo($this->imagePath, PATHINFO_EXTENSION)) . '-resized.jpg';
+        // Storage::disk('public')->put('profiles/resized/' . $resizedImageName, (string) $image->encode());
+        // $this->userProfile->profile_image = 'profiles/resized/' . $resizedImageName;
+        // $this->userProfile->save();
     }
 }
